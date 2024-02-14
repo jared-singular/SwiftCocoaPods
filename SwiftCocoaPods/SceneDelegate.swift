d4d59908d08faa09ac60bbb6ed3472c6214942f7
Jared Ornstead <jared@singular.net>
2024-02-14 11:53:00 -0800

    Updated deeplink handler function to handleDeeplink() and added attributionInfo API callback

--- SwiftCocoaPods/SceneDelegate.swift
+++ SwiftCocoaPods/SceneDelegate.swift
@@ -26,11 +26,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 config.userActivity = userActivity
                 Singular.start(config)
             }
-            
+                        
             // Request App Tracking Transparency when the App is Ready, provides IDFA on consent to Singular SDK
             Utils.requestTrackingAuthorization()
-            
-            
             
     }
     
@@ -86,23 +84,49 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
         // If your app is not displaying the App Tracking Transparency pop-up for consent, comment out the next line
         config.waitForTrackingAuthorizationWithTimeoutInterval = 300
-        config.singularLinksHandler = { params in
-            self.processDeeplink(params: params)
+        
+        // Call AttributionInfoHandler
+        config.deviceAttributionCallback = { attributionInfo in
+            self.attributionInfoHandler(attributionInfo)
         }
+        
+        // Call SingularLinkHandler
+        config.singularLinksHandler = { params in
+            self.handleDeeplink(params: params)
+        }
+        
         // Using Singular Global Properties feature to capture third party identifiers
         config.setGlobalProperty(thirdPartyKey, withValue: thirdPartyID, overrideExisting: true)
         Singular.setSessionTimeout(120)
         return config
     }
     
-    func processDeeplink(params: SingularLinkParams!) {
-        print(Date(), "-- Scene Delegate processDeeplink()")
+    func attributionInfoHandler(_ attributionInfo: [AnyHashable: Any]?) {
+        guard let attributionInfo = attributionInfo else {
+            print(Date(), "-- Singular attributionInfo is nil")
+            return
+        }
+        print(Date(), "-- Singular Attribution Info: \(attributionInfo)")
+        // Add Attribution handling code here
+    }
+    
+    func handleDeeplink(params: SingularLinkParams?) {
+        print(Date(), "-- Scene Delegate handleDeeplink()")
         
         // Get Deeplink data from Singular Link
-        let deeplink = params.getDeepLink()
-        let passthrough = params.getPassthrough()
-        let isDeferredDeeplink = params.isDeferred() ? "Yes" : "No"
-
+        let deeplink = params?.getDeepLink()
+        let passthrough = params?.getPassthrough()
+        let isDeferredDeeplink = params?.isDeferred()
+        let urlParams = params?.getUrlParameters()
+        
+        // Add deep link handling code here
+        
+        // Log SingularLinkParams
+        print(Date(), "-- Singular deeplink: \(String(describing: deeplink))")
+        print(Date(), "-- Singular passthrough: \(String(describing: passthrough))")
+        print(Date(), "-- Singular isDeferred: \(String(describing: isDeferredDeeplink))")
+        print(Date(), "-- Singular urlParams: \(String(describing: urlParams))")
+        
         // Store deeplink data in UserDefaults for access from DeeplinkController
         UserDefaults.standard.set(deeplink, forKey: Constants.DEEPLINK)
         UserDefaults.standard.set(passthrough, forKey: Constants.PASSTHROUGH)
